75ab624c2695d0e0cf879d926b17526b07023a8a
Dave Stevenson <dave.stevenson@raspberrypi.org>
2018-02-12 17:33:43 +0000

    staging: bcm2835-camera: Switch to using dma-contig allocator
    Switch from using the vmalloc vb2 allocator to the dma-contig
    one.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.org>

--- drivers/staging/vc04_services/vchiq-mmal/mmal-vchiq.c
+++ drivers/staging/vc04_services/vchiq-mmal/mmal-vchiq.c
@@ -28,7 +28,7 @@
 #include <linux/vmalloc.h>
 #include <linux/btree.h>
 #include <asm/cacheflush.h>
-#include <media/videobuf2-vmalloc.h>
+#include <media/videobuf2-dma-contig.h>
 
 #include "mmal-common.h"
 #include "mmal-vchiq.h"
